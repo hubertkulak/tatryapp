e82436e858c0303af78956dab43e4243aad6b063
maSlomiany <marysiaslomiany@gmail.com>
2023-12-28 13:06:08 +0100

    Nawigacja

--- app/src/main/java/com/example/tatryapp/TatryWysokie.kt
+++ app/src/main/java/com/example/tatryapp/TatryWysokie.kt
@@ -1,14 +1,11 @@
 package com.example.tatryapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,36 +14,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.tatryapp.ui.theme.TatryappTheme
 
-class MainActivity : ComponentActivity() {
-
-    private val viewModel by viewModels<MainViewModel> ()
-
+class TatryWysokie : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        installSplashScreen().apply{
-            setKeepOnScreenCondition{
-                !viewModel.isReady.value
-            }
-
-        }
-
         setContent {
             Column (
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
-                Button(onClick = {
-                    val navigate = Intent(this@MainActivity, TatryWysokie::class.java)
-                    startActivity(navigate)
-                }) {
-                    Text(text = "Tatry Wysokie", fontSize = 18.sp)
-                }
-
+                Text(text ="Tatry Wysokie", fontSize = 30.sp )
             }
         }
     }
